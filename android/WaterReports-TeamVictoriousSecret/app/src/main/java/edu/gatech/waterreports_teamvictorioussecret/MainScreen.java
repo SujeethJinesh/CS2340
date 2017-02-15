ed4df253d9bda91f328417667293abeca895527c
Timothy J. Aveni <timothy@tacosareawesome.com>
2017-02-15 16:05:15 -0500

    Complete M4 (tentative, untested)

---
+++ android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/MainScreen.java
@@ -0,0 +1,29 @@
+package edu.gatech.waterreports_teamvictorioussecret;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        Button logoutButton = (Button) findViewById(R.id.logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainScreen.this, WelcomeScreen.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
