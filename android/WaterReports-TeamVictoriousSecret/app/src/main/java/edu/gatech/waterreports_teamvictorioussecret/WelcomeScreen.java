4485fab4de943431c60c75d6baf601136cd13402
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-02-19 14:44:18 -0500

    Fixed Login functionality

--- android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/WelcomeScreen.java
+++ android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/WelcomeScreen.java
@@ -2,13 +2,17 @@ package edu.gatech.waterreports_teamvictorioussecret;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -52,17 +56,18 @@ public class WelcomeScreen extends AppCompatActivity {
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             // Get the layout inflater
-            LayoutInflater inflater = getActivity().getLayoutInflater();
+            final LayoutInflater inflater = getActivity().getLayoutInflater();
 
             // Inflate and set the layout for the dialog
             // Pass null as the parent view because its going in the dialog layout
-            builder.setView(inflater.inflate(R.layout.sign_in_dialog, null))
+            final View view = inflater.inflate(R.layout.sign_in_dialog, null);
+            builder.setView(view)
                     // Add action buttons
                     .setPositiveButton("Login", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int id) {
-                            EditText usernameEditText = (EditText) findViewById(R.id.username);
-                            EditText passwordEditText = (EditText) findViewById(R.id.password);
+                            EditText usernameEditText = (EditText) view.findViewById(R.id.username);
+                            EditText passwordEditText = (EditText) view.findViewById(R.id.password);
 
                             w.tryLogin(usernameEditText.getText().toString(), passwordEditText.getText().toString());
                         }
