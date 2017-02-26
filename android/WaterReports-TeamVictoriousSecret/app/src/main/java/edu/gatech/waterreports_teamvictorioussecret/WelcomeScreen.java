43b6e226b9267be9b9d79a4b4bc8cbae5fbe7810
Dhruv Mehra <dmehra6@gatech.edu>
2017-02-26 16:59:54 -0500

    Implement login and register

--- android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/WelcomeScreen.java
+++ android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/WelcomeScreen.java
@@ -9,9 +9,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -29,6 +30,15 @@ public class WelcomeScreen extends AppCompatActivity {
             }
         });
 
+        Button register = (Button) findViewById(R.id.registerButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment fragment = new RegisterDialogFragment(WelcomeScreen.this);
+                fragment.show(getFragmentManager(), "register");
+            }
+        });
+
         SocketIOSession.getInstance().addAttachListener(new Session.AttachListener() {
             @Override
             public boolean onAttachSuccess() {
@@ -39,14 +49,18 @@ public class WelcomeScreen extends AppCompatActivity {
 
             @Override
             public boolean onAttachFailure(String error) {
-                Toast.makeText(WelcomeScreen.this, error, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(WelcomeScreen.this, error, Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
     }
 
-    private void tryLogin(String username, String password) {
-        SocketIOSession.getInstance().login(username, password);
+    private void tryLogin(String email, String password) {
+        SocketIOSession.getInstance().login(email, password);
+    }
+
+    private void tryRegister(String email, String password, WorkerType type) {
+        SocketIOSession.getInstance().register(email, password, type);
     }
 
     private class LoginDialogFragment extends DialogFragment {
@@ -81,6 +95,59 @@ public class WelcomeScreen extends AppCompatActivity {
                     .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             LoginDialogFragment.this.getDialog().cancel();
+                        }
+                    });
+            return builder.create();
+        }
+    }
+
+    private class RegisterDialogFragment extends DialogFragment {
+        WelcomeScreen screen;
+
+        RegisterDialogFragment(WelcomeScreen screen) {
+            super();
+            this.screen = screen;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            // Get the layout inflater
+            final LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            final View view = inflater.inflate(R.layout.dialog_register, null);
+            Spinner spinner = (Spinner) view.findViewById(R.id.dialog_register_spinner);
+            ArrayAdapter<WorkerType> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, WorkerType.values());
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+
+            builder.setView(view)
+                    // Add action buttons
+                    .setPositiveButton(R.string.register, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            EditText emailEditText = (EditText) view.findViewById(R.id.dialog_register_email);
+                            EditText passwordEditText = (EditText) view.findViewById(R.id.dialog_register_password);
+                            EditText passwordConfirmationEditText = (EditText) view.findViewById(R.id.dialog_register_password_confirmation);
+                            Spinner workerSpinner = (Spinner) view.findViewById(R.id.dialog_register_spinner);
+                            WorkerType type = (WorkerType) workerSpinner.getSelectedItem();
+                            String email = emailEditText.getText().toString();
+                            String password = passwordEditText.getText().toString();
+                            String passwordConfirmation = passwordConfirmationEditText.getText().toString();
+
+                            if (!password.equals(passwordConfirmation)) {
+//                                Toast.makeText(getContext(), "Passwords don't match", Toast.LENGTH_SHORT).show();
+                                RegisterDialogFragment.this.getDialog().cancel();
+                            }
+
+                            screen.tryRegister(email, password, type);
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            RegisterDialogFragment.this.getDialog().cancel();
                         }
                     });
             return builder.create();
