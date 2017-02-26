9a585b4fa75f6d6d9fc10c34471d8b54d146b8fd
Dhruv Mehra <dmehra6@gatech.edu>
2017-02-26 15:54:43 -0500

    Implement User

--- android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/User.java
+++ android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/User.java
@@ -1,10 +1,27 @@
 package edu.gatech.waterreports_teamvictorioussecret;
 
-/**
- * Created by dhruvmehra on 2/26/17.
- */
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class User {
-    String username;
+    String userEmail;
     String realName;
+    WorkerType type;
+
+    public User(JSONObject obj) {
+        try {
+            this.userEmail = obj.getString("useremail");
+            this.realName = obj.getString("name");
+            this.type = WorkerType.createFromString(obj.getString("workerType"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.realName + " (" + this.userEmail + ")" + " (" + this.type + ")";
+    }
 }
