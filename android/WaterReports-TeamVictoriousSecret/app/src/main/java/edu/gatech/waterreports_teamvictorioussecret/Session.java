dc0510f41a163648d02d894e90d8d5ce18da5b3e
Dhruv Mehra <dmehra6@gatech.edu>
2017-02-26 14:08:32 -0500

    Write the Session interface

---
+++ android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/Session.java
@@ -0,0 +1,116 @@
+package edu.gatech.waterreports_teamvictorioussecret;
+
+/**
+ * Created by dhruvmehra on 2/26/17.
+ */
+
+public interface Session {
+
+    /**
+     * Gets the current session uuid, used to identify this session in network requests.
+     * Should be kept private: after attaching this session with a user, the session id is enough to
+     * send requests as the user.
+     * @return
+     */
+    String getSessionId();
+
+    /**
+     * Returns whether this Session object believes itself to be attached with a user.
+     * @return whether this Session object believes itself to be attached with a user.
+     */
+    boolean isLoggedIn();
+
+    /**
+     * Returns the current User object associated with this Session, or null if there is none.
+     * @return the current User object associated with this Session, or null if there is none.
+     */
+    User getLoggedInUser();
+
+    /**
+     * Add an AttachListener to the session object, so that when we get a response from login, we
+     * can react to it.
+     * @param listener the AttachListener object to add
+     * @return the id of this listener for this session (for later removal)
+     */
+    int addAttachListener(AttachListener listener);
+
+    /**
+     * Add a DetachListener to the session object, so that when we get a response from a logout
+     * (either forceful or voluntary), we can react to it.
+     * @param listener the AttachListener object to add
+     * @return the id of this listener for this session (for later removal)
+     */
+    int addDetachListener(DetachListener listener);
+
+    /**
+     * Removes an AttachListener from the session object
+     * @param listenerId the id of the added listener
+     * @return whether a listener with this id was found and removed
+     */
+    boolean removeListener(int listenerId);
+
+    /**
+     * Attempts to log into the server, attaching this session with a user.
+     * When a response is returned, all AttachListeners added to the session will be notified.
+     * @param username the user's username
+     * @param password the user's password
+     */
+    void login(String username, String password);
+
+    /**
+     * Attempts to create an account on the server. If successful, this will attach this session
+     * with the newly-created user.
+     * When a response is returned, all AttachListeners added to the session will be notified.
+     * @param username the new user's username
+     * @param password the new user's password
+     */
+    void register(String username, String password);
+
+    /**
+     * Sends a request to the server to invalidate the session attachment between the session and
+     * its current user.
+     * When a response is returned, all DetachListeners added to the session will be notified.
+     */
+    void logout();
+
+    interface AttachListener {
+        /**
+         * Will be called when a successful login-attach response is received from the server.
+         * Listeners are called in order of being added.
+         * No parameters or event data is associated; all the relevant information will be reflected
+         * in changes to the Session object.
+         * @return whether the event should continue to propagate to the following listeners
+         */
+        boolean onAttachSuccess();
+
+        /**
+         * Will be called when a failed login-attach response is received from the server or no
+         * response is received from the server.
+         * Listeners are called in order of being added.
+         * @param error A string containing information about the error
+         * @return whether the event should continue to propagate to the following listeners
+         */
+        boolean onAttachFailure(String error);
+    }
+
+    interface DetachListener {
+        /**
+         * Will be called when a successful detach response is received from the server. This might
+         * happen because of a logout() call from the client OR because the server has invalidated
+         * the session (for example, because another session was attached with this user).
+         * Listeners are called in order of being added.
+         * No parameters or event data is associated; all the relevant information will be reflected
+         * in changes to the Session object.
+         * @return whether the event should continue to propagate to the following listeners
+         */
+        boolean onDetachSuccess();
+
+        /**
+         * Will be called when a failed detach response is received from the server or no
+         * response is received from the server.
+         * @param error A string containing information about the error
+         * @return whether the event should continue to propagate to the following listeners
+         */
+        boolean onDetachFailure(String error);
+    }
+}
