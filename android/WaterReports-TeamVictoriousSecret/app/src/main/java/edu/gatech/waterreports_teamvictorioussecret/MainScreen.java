d159b32117f2e88263dae72432ec93b323ef5da0
Dhruv Mehra <dmehra6@gatech.edu>
2017-02-26 16:59:32 -0500

    Implement Logout

--- android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/MainScreen.java
+++ android/WaterReports-TeamVictoriousSecret/app/src/main/java/edu/gatech/waterreports_teamvictorioussecret/MainScreen.java
@@ -2,10 +2,7 @@ package edu.gatech.waterreports_teamvictorioussecret;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,13 +12,25 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-
         Button logoutButton = (Button) findViewById(R.id.logout);
+
+        SocketIOSession.getInstance().addDetachListener(new Session.DetachListener() {
+            @Override
+            public boolean onDetachSuccess() {
+                Intent intent = new Intent(MainScreen.this, WelcomeScreen.class);
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public boolean onDetachFailure(String error) {
+                return false;
+            }
+        });
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, WelcomeScreen.class);
-                startActivity(intent);
+                SocketIOSession.getInstance().logout();
             }
         });
     }
